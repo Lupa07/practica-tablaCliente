ca5b4cc9863c8fe3294a3fa2d19fdf788d2ec38e
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-03-30 09:47:24 +0200

    generar componenetes y colocarlos en root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FiltroComponent } from './filtro/filtro.component';
+import { TablaDatosComponent } from './tabla-datos/tabla-datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FiltroComponent,
+    TablaDatosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tabla-datos/tabla-datos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-datos',
+  templateUrl: './tabla-datos.component.html',
+  styleUrls: ['./tabla-datos.component.scss']
+})
+export class TablaDatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
