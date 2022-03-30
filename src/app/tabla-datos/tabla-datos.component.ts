ae07c83834e6bfe75c869406cb8e0dd0a72b3209
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-03-30 21:19:19 +0200

    to colocaito

--- src/app/tabla-datos/tabla-datos.component.ts
+++ src/app/tabla-datos/tabla-datos.component.ts
@@ -14,5 +14,54 @@ export class TablaDatosComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  extraerhora(fecha: String) {
+    let hora_extraida = fecha.substring(11, 16);
+    return hora_extraida;
+  }
 
+  extraerfecha(fecha: String) {
+    let fecha_extraida = fecha.substring(9, 10);
+    let mes_numero = fecha.substring(5, 7);
+    console.log(mes_numero);
+    switch (mes_numero) {
+      case '01':
+        fecha_extraida = fecha_extraida + ' enero';
+        break;
+      case '02':
+        fecha_extraida = fecha_extraida + ' febrero';
+        break;
+      case '03':
+        fecha_extraida = fecha_extraida + ' marzo';
+        break;
+      case '04':
+        fecha_extraida = fecha_extraida + ' abril';
+        break;
+      case '05':
+        fecha_extraida = fecha_extraida + ' mayo';
+        break;
+      case '06':
+        fecha_extraida = fecha_extraida + ' junio';
+        break;
+      case '07':
+        fecha_extraida = fecha_extraida + ' julio';
+        break;
+      case '08':
+        fecha_extraida = fecha_extraida + ' sept';
+        break;
+      case '09':
+        fecha_extraida = fecha_extraida + ' oct';
+        break;
+      case '10':
+        fecha_extraida = fecha_extraida + ' nov';
+        break;
+      case '11':
+        fecha_extraida = fecha_extraida + ' dic';
+        break;
+      case '12':
+        fecha_extraida = fecha_extraida + ' enero';
+        break;
+    }
+    fecha_extraida = fecha_extraida + ' ' + fecha.substring(2, 4);
+    return fecha_extraida;
+  }
 }
